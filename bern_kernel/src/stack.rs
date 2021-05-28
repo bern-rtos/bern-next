405e6d18cacbc5777dc6c2fe4581648cce8b4a6b
Stefan Lüthi <stefan.luethi@outlook.com>
2021-05-28 17:27:22 +0200

    Align task stacks for MPU

--- bern_kernel/src/stack.rs
+++ bern_kernel/src/stack.rs
@@ -1,3 +1,4 @@
+use core::ops::{DerefMut, Deref};
 
 #[derive(Copy, Clone)] // todo: remove
 #[repr(C)]
@@ -25,19 +26,56 @@ impl Stack {
     }
 }
 
+
+// based on https://github.com/japaric/aligned/blob/master/src/lib.rs
+#[repr(align(512))]
+pub struct A256B;
+
+#[repr(C)]
+pub struct Aligned<A, T>
+    where
+        T: ?Sized,
+{
+    _alignment: [A; 0],
+    value: T,
+}
+
+#[allow(non_snake_case)]
+pub const fn Aligned<A, T>(value: T) -> Aligned<A, T> {
+    Aligned {
+        _alignment: [],
+        value,
+    }
+}
+
+impl<A, T> Deref for Aligned<A, T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.value
+    }
+}
+impl<A, T> DerefMut for Aligned<A, T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.value
+    }
+}
+
 // todo: enforce alignment and size restrictions
 #[macro_export]
 macro_rules! alloc_static_stack {
     ($size:expr) => {
         {
             #[link_section = ".task_stack"]
-            static mut STACK: [u8; $size] = [0; $size]; // will not be initialized -> linker script
-            unsafe{ // stack pattern for debugging
-                for byte in STACK.iter_mut() {
+            static mut STACK: $crate::stack::Aligned<$crate::stack::A256B, [u8; $size]> =
+                $crate::stack::Aligned([0; $size]);
+            //static mut STACK: [u8; $size] = [0; $size]; // will not be initialized -> linker script
+            /*unsafe{ // stack pattern for debugging
+                for byte in *STACK.iter_mut() {
                     *byte = 0xAA;
                 }
-            }
-            unsafe { $crate::stack::Stack::new(STACK.as_mut()) }
+            }*/
+            unsafe { $crate::stack::Stack::new(&mut *STACK) }
         }
     };
 }
