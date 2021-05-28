631a70952ec5a3f5fd34fb2ff07e50e579e80a8a
Stefan Lüthi <stefan.luethi@outlook.com>
2021-05-28 17:28:53 +0200

    Provide memory region information from linker script

--- bern_arch/src/cortex_m/startup.rs
+++ bern_arch/src/cortex_m/startup.rs
@@ -1,16 +1,28 @@
 use crate::IStartup;
 use crate::arch::Arch;
 use r0;
+use crate::startup::Region;
+
+extern "C" {
+    static mut __sshared: u32;
+    static mut __eshared: u32;
+    static __sishared: u32;
+}
 
 impl IStartup for Arch {
     fn init_static_memory() {
-        extern "C" {
-            static mut __sshared: u32;
-            static mut __eshared: u32;
-            static __sishared: u32;
-        }
         unsafe {
-            r0::init_data(&mut __sshared, &mut __eshared, &__sishared);
+            let mut shared_ptr = __sshared;
+            r0::init_data(&mut shared_ptr, &mut __eshared, &__sishared);
+        }
+    }
+
+    fn region() -> Region {
+        unsafe {
+            Region {
+                start: __sshared as *const _,
+                stop: __eshared as *const _,
+            }
         }
     }
 }
--- bern_arch/src/startup.rs
+++ bern_arch/src/startup.rs
@@ -1,3 +1,10 @@
+#[derive(Copy, Clone)]
+pub struct Region {
+    pub start: *const usize,
+    pub stop: *const usize,
+}
+
 pub trait IStartup {
     fn init_static_memory();
+    fn region() -> Region;
 }
