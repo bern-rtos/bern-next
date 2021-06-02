5d0483d6331caf57d5843b1011ce54b7dd13e180
Stefan Lüthi <stefan.luethi@outlook.com>
2021-06-02 17:17:54 +0200

    Add enum map macro

--- bern_kernel/macros/src/lib.rs
+++ bern_kernel/macros/src/lib.rs
@@ -1,12 +1,12 @@
-mod conf;
+mod enum_map;
 
 use proc_macro::{TokenStream};
 use quote::ToTokens;
 
 #[proc_macro]
-pub fn load_conf(input: TokenStream) -> TokenStream {
-    let config = syn::parse_macro_input!(input as conf::Config);
+pub fn enum_map(input: TokenStream) -> TokenStream {
+    let map = syn::parse_macro_input!(input as enum_map::EnumMap);
     let mut output = proc_macro2::TokenStream::new();
-    config.to_tokens(&mut output);
+    map.to_tokens(&mut output);
     TokenStream::from(output)
 }
